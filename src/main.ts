a20acfa4ea6f641feb2e5a4c6d233e7abb632c9c
Ariel Zimbrão <aquilazimbrao@hotmail.com>
2020-05-24 18:59:09 -0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-require('dotenv').config()
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter()
-  );
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
